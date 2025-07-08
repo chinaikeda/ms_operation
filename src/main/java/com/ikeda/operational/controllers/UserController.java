a41df2ff62c92218d7304910d089e1483335cf54
Agenor Ikeda <china.ikeda@gmail.com>
2025-07-07 22:34:53 -0300

    Token entre entre MS Authuser e MS Operational

--- src/main/java/com/ikeda/operational/controllers/UserController.java
+++ src/main/java/com/ikeda/operational/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.ikeda.operational.controllers;
 
+import com.ikeda.operational.configs.security.AuthenticationCurrentUserService;
+import com.ikeda.operational.configs.security.UserDetailsImpl;
 import com.ikeda.operational.models.UserModel;
 import com.ikeda.operational.services.UserService;
 import com.ikeda.operational.specifications.SpecificationTemplate;
@@ -10,6 +12,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -21,14 +27,22 @@ public class UserController {
     Logger logger = LogManager.getLogger(UserController.class);
 
     final UserService userService;
+    final AuthenticationCurrentUserService authenticationCurrentUserService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationCurrentUserService authenticationCurrentUserService) {
         this.userService = userService;
+        this.authenticationCurrentUserService = authenticationCurrentUserService;
     }
 
+
+    @PreAuthorize("hasAnyRole('MANAGER')")
     @GetMapping
     public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec,
-                                                       Pageable pageable){
+                                                       Pageable pageable,
+                                                       Authentication authentication){
+        UserDetails userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        logger.info("Authentication {}", userDetails.getUsername());
+
         Page<UserModel> userModelPage = userService.findAll(spec, pageable);
         if (!userModelPage.isEmpty()){
             for (UserModel user: userModelPage.toList()){
@@ -38,8 +52,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
+    @PreAuthorize("hasAnyRole('USER')")
     @GetMapping("/{userId}")
     public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId){
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(userId).get());
+        UUID currentUserId = authenticationCurrentUserService.getCurrrentUser().getUserId();
+        if (currentUserId.equals(userId)){
+            return ResponseEntity.status(HttpStatus.OK).body(userService.findById(userId).get());
+        } else {
+            throw new AccessDeniedException("Forbidden");
+        }
     }
 }
